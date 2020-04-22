b52b0a2e2d725e4352c4db0e913145e71ce1cc31
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-04-22 18:11:58 +0200

    4.1. Add card to form swaping behavior

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Board from './components/board.js';
-import Card from './components/card.js';
-import Filters from './components/filters.js';
-import Form from './components/form.js';
-import LoadMoreButton from './components/load-more.js';
-import Menu from './components/menu.js';
-import Sort from './components/sort.js';
-import Tasks from './components/tasks.js';
+import BoardComponent from './components/board.js';
+import CardComponent from './components/card.js';
+import FiltersComponent from './components/filters.js';
+import FormComponent from './components/form.js';
+import LoadMoreButtonComponent from './components/load-more.js';
+import MenuComponent from './components/menu.js';
+import SortComponent from './components/sort.js';
+import TasksComponent from './components/tasks.js';
 
 import {filters} from './mocks/filters';
 import {generateTasksMock} from './mocks/tasks';
@@ -19,37 +19,57 @@ const TASKS_ON_CLICK_COUNT = 8;
 const tasks = generateTasksMock(TASKS_COUNT);
 
 
+const renderTask = (tasksContainer, task) => {
+  const onEditButtonClick = () => {
+    tasksContainer.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
+  };
+
+  const onEditFormSubmit = (evt) => {
+    evt.preventDefault();
+    tasksContainer.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
+  };
+
+  const taskComponent = new CardComponent(task);
+  const editButton = taskComponent.getElement().querySelector(`.card__btn--edit`);
+  editButton.addEventListener(`click`, onEditButtonClick);
+
+  const taskEditComponent = new FormComponent(task);
+  const editForm = taskEditComponent.getElement().querySelector(`form`);
+  editForm.addEventListener(`submit`, onEditFormSubmit);
+
+  render(tasksContainer, taskComponent.getElement());
+};
+
+
 const mainControl = document.querySelector(`.main__control`);
-const menu = new Menu().getElement();
-render(mainControl, menu);
+render(mainControl, new MenuComponent().getElement());
 
 const mainElement = document.querySelector(`.main`);
-render(mainElement, new Filters(filters).getElement());
+render(mainElement, new FiltersComponent(filters).getElement());
 
-const board = new Board().getElement();
+const board = new BoardComponent().getElement();
 render(mainElement, board);
 
-const sort = new Sort().getElement();
+const sort = new SortComponent().getElement();
 render(board, sort, `afterbegin`);
 
-const tasksContainer = new Tasks().getElement();
+const tasksContainer = new TasksComponent().getElement();
 render(board, tasksContainer);
-const taskEditForm = new Form(tasks[0]).getElement();
-render(tasksContainer, taskEditForm);
+
 
 let tasksShownCount = TASKS_ON_START_COUNT;
 
-tasks.slice(1, tasksShownCount)
-    .forEach((task) => render(tasksContainer, new Card(task).getElement()));
+tasks.slice(0, tasksShownCount)
+    .forEach((task) => renderTask(tasksContainer, task));
 
-const loadMoreButton = new LoadMoreButton().getElement();
+const loadMoreButton = new LoadMoreButtonComponent().getElement();
 render(board, loadMoreButton);
 
 loadMoreButton.addEventListener(`click`, () => {
   const prevTasksShownCount = tasksShownCount;
   tasksShownCount += TASKS_ON_CLICK_COUNT;
   tasks.slice(prevTasksShownCount, tasksShownCount)
-    .forEach((task) => render(tasksContainer, new Card(task).getElement()));
+    .forEach((task) => renderTask(tasksContainer, task));
 
   if (tasksShownCount >= tasks.length) {
     loadMoreButton.remove();
